787d130e5615b589a3e4fe731e12c8d5a3476e4a
Stearinrin <jacker384sushilover@gmail.com>
2020-07-15 01:22:24 +0900

    challenge1 (face detection)

---
+++ Challenge1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Challenge1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(338, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(168, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Camera Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Camera Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(338, 260);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(320, 19);
+            this.textBox1.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 288);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Face Capture";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Challenge1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+//using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+using OpenCvSharp.Extensions;
+
+namespace Challenge1
+{
+    public partial class Form1 : Form
+    {
+        private Mat queryFrame = new Mat();
+        private VideoCapture videoCapture;
+        private Size formSize;
+        private int width, height;
+
+        // github上のopencv/opencvを実行可能ファイルと同じディレクトリでクローン
+        // Haar-likeカスケード型検出器の読み込み
+        private CascadeClassifier cascade = new CascadeClassifier(
+            "./opencv/data/haarcascades/haarcascade_frontalface_default.xml");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // カメラの起動
+            videoCapture = new VideoCapture(0);
+            if (!videoCapture.IsOpened())
+            {
+                MessageBox.Show("Camera was not found", "Face Capture", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+
+            // ビデオキャプチャするカメラのサイズ
+            width = videoCapture.FrameWidth;
+            height = videoCapture.FrameHeight;
+
+            // デフォルトのカメラサイズ
+            formSize = new Size(pictureBox1.Width, pictureBox1.Height);
+
+            //textBox1.Text = videoCapture.Fps.ToString();
+            //timer1.Interval = (int)(1000 / videoCapture.Fps);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // スタート・再開時の処理
+            timer1.Start();
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 一時停止時の処理
+            timer1.Stop();
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                // 画像取得
+                videoCapture.Retrieve(queryFrame, CameraChannels.OpenNI_BGRImage);
+
+                // 320 x 240へリサイズ
+                Cv2.Resize(queryFrame, queryFrame, formSize);
+
+                // キャプチャ画像表示
+                pictureBox1.Image = queryFrame.ToBitmap();
+
+                // 顔認識
+                pictureBox2.Image = faceDetect(ref queryFrame).ToBitmap();
+            }
+            catch (OpenCVException)
+            {
+                // アプリの終了
+                timer1.Stop();
+                MessageBox.Show("Camera was not found", "Face Capture", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+        }
+
+        private Mat faceDetect(ref Mat image)
+        {
+            // 顔と認識した箇所をRect配列に格納
+            Rect[] rects = cascade.DetectMultiScale(
+                image, 1.1, 3, HaarDetectionType.Zero, 
+                new OpenCvSharp.CPlusPlus.Size(30, 30));
+
+            // Rectを描画
+            foreach (Rect rect in rects)
+            {
+                Cv2.Rectangle(image, rect, new Scalar(0, 0, 255), 2);
+            }
+
+            // 検出数を表示
+            int detects = rects.Length;
+            textBox1.Text = (detects > 0 ? detects.ToString() : "No") + " face" + (detects > 1 ? "s" : "") + " detected";
+
+            return image;
+        }
+    }
+}
